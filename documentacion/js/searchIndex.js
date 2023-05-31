8f66f732f3a17c3b2b4f6061f8320177685ff80c
NoemiMSotelo <135159053+NoemiMSotelo@users.noreply.github.com>
2023-05-31 20:28:16 +0200

    Add files via upload

---
+++ documentacion/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\longitud_cadena\u0028\u0029",
+            "name": "longitud_cadena",
+            "summary": "Funci\u00F3n\u0020para\u0020obtener\u0020la\u0020longitud\u0020de\u0020una\u0020cadena.",
+            "url": "namespaces/default.html#function_longitud_cadena"
+        },                {
+            "fqsen": "\\invertir_cadena\u0028\u0029",
+            "name": "invertir_cadena",
+            "summary": "Funci\u00F3n\u0020para\u0020invertir\u0020\u0020una\u0020cadena.",
+            "url": "namespaces/default.html#function_invertir_cadena"
+        },                {
+            "fqsen": "\\Calculadora",
+            "name": "Calculadora",
+            "summary": "Clase\u0020que\u0020representa\u0020una\u0020calculadora\u0020de\u0020suma\u0020y\u0020resta.",
+            "url": "classes/Calculadora.html"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003ASuma\u0028\u0029",
+            "name": "Suma",
+            "summary": "M\u00E9todo\u0020para\u0020sumar\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_Suma"
+        },                {
+            "fqsen": "\\Calculadora\u003A\u003AResta\u0028\u0029",
+            "name": "Resta",
+            "summary": "M\u00E9todo\u0020para\u0020restar\u0020dos\u0020n\u00FAmeros.",
+            "url": "classes/Calculadora.html#method_Resta"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
